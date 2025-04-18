0fdb1f01bda87b0ad668d94c6fb361d061b2e916
Ilya <goconocy@gmail.com>
2025-04-18 03:29:12 +0300

    Initial commit

--- main.py
+++ main.py
@@ -44,10 +44,6 @@ settings = {
     # Вывод отладочных сообщений (Показывает все строки, которые отправляются в переводчик)
     'translator_debug': True,
 }
-
-# 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
-# io-v2-eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJvd25lciI6ImZjYzQ4MzVjLTBjNGYtNDJjMi05NjVjLWE0ZDYwZDI3M2M0OCIsImV4cCI6NDg5NzA0OTI5NX0.FYbFHs1BYbT1XnMn8psG_cT1lISoRWYIzNjx8WObWZu0kNRrb9LW0QMHrtzO3wM3742syyfSqLvNnKKYAKErNg
-
 if __name__ == "__main__":
     from TranslationManager import TranslationManager
     mod_translator = TranslationManager(settings)
